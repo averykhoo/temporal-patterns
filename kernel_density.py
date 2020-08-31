a839489d81da7b995eeb08dd06a8d997551a9376
Avery <averykhoo@gmail.com>
2020-08-31 18:14:18 +0800

    better plotting graphs

--- kernel_density.py
+++ kernel_density.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
+import scipy.special
+import scipy.stats
 from sklearn.neighbors import KernelDensity
 
 N = 1000
@@ -8,19 +9,23 @@ np.random.seed(1)
 X1 = np.concatenate((np.random.normal(15, 10, int(0.3 * N)),
                      np.random.normal(75, 20, int(0.7 * N))))[:, np.newaxis]
 
-X2 = np.concatenate((np.random.normal(35, 5, int(0.4 * N)),
-                     np.random.normal(55, 10, int(0.6 * N))))[:, np.newaxis]
+X2 = np.concatenate((np.random.normal(25, 15, int(0.4 * N)),
+                     np.random.normal(45, 10, int(0.6 * N))))[:, np.newaxis]
 
 
-def draw_histograms(data, labels, min_val=0.0, max_val=100, num_bins=60, font_size=11):
-
+def draw_histograms(data, labels, min_val=0.0, max_val=100, num_bins=60, bandwidth=5):
     assert len(labels) == len(data)
 
     plt.cla()
     plt.clf()
     plt.close()
 
-    fig, axes = plt.subplots(nrows=len(data), ncols=1)
+    _x_axis_label = 'score'
+    _y_axis_label = 'count'
+    _axis_font_size = 11
+    _data_label_font_size = _axis_font_size + 2  # title in large font
+
+    fig, axes = plt.subplots(nrows=len(data))  # , ncols=1)
 
     # sorts by x_mean
     for (x_mean, label, x), axis in zip(sorted(zip(map(np.mean, data), labels, data)),
@@ -32,10 +37,13 @@ def draw_histograms(data, labels, min_val=0.0, max_val=100, num_bins=60, font_si
         # plot histogram
         n, bins, patches = axis.hist(x, num_bins, density=0, range=(min_val, max_val), color='blue', alpha=0.2,
                                      label='histogram')
+        assert len(bins) - 1 == len(n) == num_bins, (len(bins), len(n), num_bins)
+        assert sum(n) == len(x)
 
         plot_range = np.linspace(min_val, max_val, 1000)[:, np.newaxis]
         for k in ['gaussian', 'tophat', 'linear', 'epanechnikov', 'exponential', 'cosine']:
-            kde = KernelDensity(kernel=k, bandwidth=5).fit(x)
+            kde = KernelDensity(kernel=k, bandwidth=bandwidth)
+            kde.fit(x)
             log_dens = kde.score_samples(plot_range)
             plt_vals = np.exp(log_dens) * (len(x) * (float(max_val - min_val) / num_bins))
             plt_vals[0] = 0
@@ -45,24 +53,22 @@ def draw_histograms(data, labels, min_val=0.0, max_val=100, num_bins=60, font_si
         # plot mean
         axis.axvline(x=x_mean, color='black', linestyle='-', label='mean')
 
-        # transform data to logit space
-        l_data = scipy.special.logit((x - min_val) / (max_val - min_val))
-        l_mean = np.mean(l_data)
-        l_sigma = np.sqrt(np.var(l_data))
-        l_range = (l_mean - 4 * l_sigma, l_mean + 4 * l_sigma)
-
-        # plot normal distribution in logit space
-        l_curve_x = np.linspace(l_range[0], l_range[1], 1000)  # 1000 plot points
-        l_curve_y = len(l_data) * ((l_range[1] - l_range[0]) / num_bins) * scipy.stats.norm.pdf(l_curve_x, l_mean,
-                                                                                                l_sigma)
-
-        # transform back into real space
-        axis.plot((scipy.special.expit(l_curve_x) * (max_val - min_val)) + min_val,
-                  l_curve_y,
-                  color='black',
-                  linestyle='-',
-                  linewidth=0.5,
-                  label='logit-norm')
+        # # transform data to logit space
+        # # import scipy.special
+        # l_data = scipy.special.logit((x - min_val) / (max_val - min_val))
+        # l_mean = np.mean(l_data)
+        # l_sigma = np.sqrt(np.var(l_data))
+        # l_range = (l_mean - 4 * l_sigma, l_mean + 4 * l_sigma)
+        #
+        # # plot normal distribution in logit space
+        # l_curve_x = np.linspace(l_range[0], l_range[1], 1000)  # 1000 plot points
+        # l_curve_y = len(l_data) * ((l_range[1] - l_range[0]) / num_bins) * \
+        #             scipy.stats.norm.pdf(l_curve_x, l_mean, l_sigma)
+        #
+        # # transform back into real space
+        # # WARNING: the area under the transformed curve is NOT normalized
+        # curve_x = (scipy.special.expit(l_curve_x) * (max_val - min_val)) + min_val
+        # axis.plot(curve_x, l_curve_y, color='black', linestyle='-', linewidth=0.5, label='logit-norm')
 
         # skew-normal
         x2_skew = scipy.stats.skew(x)
@@ -84,14 +90,14 @@ def draw_histograms(data, labels, min_val=0.0, max_val=100, num_bins=60, font_si
         # axis.axvspan(min_val, cut, color='0.4', alpha=0.5)
 
         # labels and formatting
-        axis.set_title(label, fontsize=font_size + 2)
-        axis.set_ylabel('count', fontsize=font_size)
-        axis.set_xlabel('score', fontsize=font_size)
+        axis.set_title(label, fontsize=_data_label_font_size)
+        axis.set_ylabel(_y_axis_label, fontsize=_axis_font_size)
+        axis.set_xlabel(_x_axis_label, fontsize=_axis_font_size)
         axis.grid(True)
         for tick_label in axis.get_yticklabels():
-            tick_label.set_fontsize(font_size - 1)
+            tick_label.set_fontsize(_axis_font_size - 1)
         for tick_label in axis.get_xticklabels():
-            tick_label.set_fontsize(font_size - 1)
+            tick_label.set_fontsize(_axis_font_size - 1)
 
         # fix y tick label to be actual amounts not probabilities
         tick_labels = [int(tick) if i % 4 == 0 else '' for i, tick in enumerate(axis.get_yticks())]
