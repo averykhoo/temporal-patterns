3861ba0e4d93353d2bd98449843ff945d2657c5b
Avery <averykhoo@gmail.com>
2020-08-31 18:01:31 +0800

    plotting graphs

--- kernel_density.py
+++ kernel_density.py
@@ -5,41 +5,48 @@ from sklearn.neighbors import KernelDensity
 
 N = 1000
 np.random.seed(1)
-X = np.concatenate((np.random.normal(15, 10, int(0.3 * N)),
-                    np.random.normal(75, 20, int(0.7 * N))))[:, np.newaxis]
-X = np.array([x[0] for x in X if 0 <= x <= 100]).reshape(-1, 1)
+X1 = np.concatenate((np.random.normal(15, 10, int(0.3 * N)),
+                     np.random.normal(75, 20, int(0.7 * N))))[:, np.newaxis]
+
+X2 = np.concatenate((np.random.normal(35, 5, int(0.4 * N)),
+                     np.random.normal(55, 10, int(0.6 * N))))[:, np.newaxis]
 
 
-def draw_histograms(data, labels, num_bins=40, font_size=11):
+def draw_histograms(data, labels, min_val=0.0, max_val=100, num_bins=60, font_size=11):
+
     assert len(labels) == len(data)
-    min_val = 0.0
-    max_val = 100.0
 
     plt.cla()
     plt.clf()
     plt.close()
 
     fig, axes = plt.subplots(nrows=len(data), ncols=1)
+
+    # sorts by x_mean
     for (x_mean, label, x), axis in zip(sorted(zip(map(np.mean, data), labels, data)),
                                         axes.flatten() if len(data) > 1 else [axes]):
+
+        # filter to min/max
+        x = np.array([elem[0] for elem in x if min_val <= elem <= max_val]).reshape(-1, 1)
+
         # plot histogram
         n, bins, patches = axis.hist(x, num_bins, density=0, range=(min_val, max_val), color='blue', alpha=0.2,
-                                     label=label)
+                                     label='histogram')
 
         plot_range = np.linspace(min_val, max_val, 1000)[:, np.newaxis]
         for k in ['gaussian', 'tophat', 'linear', 'epanechnikov', 'exponential', 'cosine']:
-            kde = KernelDensity(kernel=k, bandwidth=3).fit(X)
+            kde = KernelDensity(kernel=k, bandwidth=5).fit(x)
             log_dens = kde.score_samples(plot_range)
             plt_vals = np.exp(log_dens) * (len(x) * (float(max_val - min_val) / num_bins))
             plt_vals[0] = 0
             plt_vals[-1] = 0
-            axis.plot(plot_range[:, 0], plt_vals, label=k)
+            axis.plot(plot_range[:, 0], plt_vals, linewidth=1, label=k)
 
         # plot mean
-        axis.axvline(x=x_mean, color='black', linestyle='--')
+        axis.axvline(x=x_mean, color='black', linestyle='-', label='mean')
 
         # transform data to logit space
-        l_data = scipy.special.logit(X / 100)
+        l_data = scipy.special.logit((x - min_val) / (max_val - min_val))
         l_mean = np.mean(l_data)
         l_sigma = np.sqrt(np.var(l_data))
         l_range = (l_mean - 4 * l_sigma, l_mean + 4 * l_sigma)
@@ -50,7 +57,11 @@ def draw_histograms(data, labels, num_bins=40, font_size=11):
                                                                                                 l_sigma)
 
         # transform back into real space
-        axis.plot(scipy.special.expit(l_curve_x) * 100, l_curve_y, color='black', linestyle='-', linewidth=1,
+        axis.plot((scipy.special.expit(l_curve_x) * (max_val - min_val)) + min_val,
+                  l_curve_y,
+                  color='black',
+                  linestyle='-',
+                  linewidth=0.5,
                   label='logit-norm')
 
         # skew-normal
@@ -67,12 +78,10 @@ def draw_histograms(data, labels, num_bins=40, font_size=11):
         n_plot = len(x) * (max_val / num_bins) * scipy.stats.norm.pdf(curve_x, x_mean, np.sqrt(np.var(x)))
         axis.plot(curve_x, n_plot, color='black', linestyle='-.', linewidth=1, label='gaussian')
 
-        # shade cutoff area
-        cut = 30  # score at which to cut off
-        axis.axvline(x=cut, color='0.7', linestyle='-')
-        axis.axvspan(0, cut, color='0.4', alpha=0.5)
-        count_remaining = sum(val > cut for val in x)
-        label += ': %d/%d (%0.2f%%) remaining' % (count_remaining, len(x), count_remaining * 100.0 / len(x))
+        # # shade cutoff area
+        # cut = 30  # score at which to cut off
+        # # axis.axvline(x=cut, color='0.7', linestyle='-')
+        # axis.axvspan(min_val, cut, color='0.4', alpha=0.5)
 
         # labels and formatting
         axis.set_title(label, fontsize=font_size + 2)
@@ -98,4 +107,4 @@ def draw_histograms(data, labels, num_bins=40, font_size=11):
     plt.close()
 
 
-draw_histograms([X], ['x'])
+draw_histograms([X1, X2], ['x1', 'x2'])
