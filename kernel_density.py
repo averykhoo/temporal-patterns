ad5763e8072619372e8276f7bcf38a6578b64fac
Avery <averykhoo@gmail.com>
2020-08-31 17:12:56 +0800

    plotting graphs

--- kernel_density.py
+++ kernel_density.py
@@ -1,118 +1,101 @@
-# Author: Jake Vanderplas <jakevdp@cs.washington.edu>
-#
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+import numpy as np
+import scipy
 from sklearn.neighbors import KernelDensity
 
-# ----------------------------------------------------------------------
-# Plot the progression of histograms to kernels
+N = 1000
 np.random.seed(1)
-N = 20
-X = np.concatenate((np.random.normal(0, 1, int(0.3 * N)),
-                    np.random.normal(5, 1, int(0.7 * N))))[:, np.newaxis]
-X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
-bins = np.linspace(-5, 10, 10)
+X = np.concatenate((np.random.normal(15, 10, int(0.3 * N)),
+                    np.random.normal(75, 20, int(0.7 * N))))[:, np.newaxis]
+X = np.array([x[0] for x in X if 0 <= x <= 100]).reshape(-1, 1)
 
-fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
-fig.subplots_adjust(hspace=0.05, wspace=0.05)
 
-# histogram 1
-ax[0, 0].hist(X[:, 0], bins=bins, fc='#AAAAFF', density= True)
-ax[0, 0].text(-3.5, 0.31, "Histogram")
+def draw_histograms(data, labels, num_bins=40, font_size=11):
+    assert len(labels) == len(data)
+    min_val = 0.0
+    max_val = 100.0
 
-# histogram 2
-ax[0, 1].hist(X[:, 0], bins=bins + 0.75, fc='#AAAAFF', density= True)
-ax[0, 1].text(-3.5, 0.31, "Histogram, bins shifted")
+    plt.cla()
+    plt.clf()
+    plt.close()
 
-# tophat KDE
-kde = KernelDensity(kernel='tophat', bandwidth=0.75).fit(X)
-log_dens = kde.score_samples(X_plot)
-ax[1, 0].fill(X_plot[:, 0], np.exp(log_dens), fc='#AAAAFF')
-ax[1, 0].text(-3.5, 0.31, "Tophat Kernel Density")
+    fig, axes = plt.subplots(nrows=len(data), ncols=1)
+    for (x_mean, label, x), axis in zip(sorted(zip(map(np.mean, data), labels, data)),
+                                        axes.flatten() if len(data) > 1 else [axes]):
+        # plot histogram
+        n, bins, patches = axis.hist(x, num_bins, density=0, range=(min_val, max_val), color='blue', alpha=0.2,
+                                     label=label)
 
-# Gaussian KDE
-kde = KernelDensity(kernel='gaussian', bandwidth=0.75).fit(X)
-log_dens = kde.score_samples(X_plot)
-ax[1, 1].fill(X_plot[:, 0], np.exp(log_dens), fc='#AAAAFF')
-ax[1, 1].text(-3.5, 0.31, "Gaussian Kernel Density")
+        plot_range = np.linspace(min_val, max_val, 1000)[:, np.newaxis]
+        for k in ['gaussian', 'tophat', 'linear', 'epanechnikov', 'exponential', 'cosine']:
+            kde = KernelDensity(kernel=k, bandwidth=3).fit(X)
+            log_dens = kde.score_samples(plot_range)
+            plt_vals = np.exp(log_dens) * (len(x) * (float(max_val - min_val) / num_bins))
+            plt_vals[0] = 0
+            plt_vals[-1] = 0
+            axis.plot(plot_range[:, 0], plt_vals, label=k)
 
-for axi in ax.ravel():
-    axi.plot(X[:, 0], np.full(X.shape[0], -0.01), '+k')
-    axi.set_xlim(-4, 9)
-    axi.set_ylim(-0.02, 0.34)
+        # plot mean
+        axis.axvline(x=x_mean, color='black', linestyle='--')
 
-for axi in ax[:, 0]:
-    axi.set_ylabel('Normalized Density')
+        # transform data to logit space
+        l_data = scipy.special.logit(X / 100)
+        l_mean = np.mean(l_data)
+        l_sigma = np.sqrt(np.var(l_data))
+        l_range = (l_mean - 4 * l_sigma, l_mean + 4 * l_sigma)
 
-for axi in ax[1, :]:
-    axi.set_xlabel('x')
+        # plot normal distribution in logit space
+        l_curve_x = np.linspace(l_range[0], l_range[1], 1000)  # 1000 plot points
+        l_curve_y = len(l_data) * ((l_range[1] - l_range[0]) / num_bins) * scipy.stats.norm.pdf(l_curve_x, l_mean,
+                                                                                                l_sigma)
 
-# ----------------------------------------------------------------------
-# # Plot all available kernels
-# X_plot = np.linspace(-6, 6, 1000)[:, None]
-# X_src = np.zeros((1, 1))
-#
-# fig, ax = plt.subplots(2, 3, sharex=True, sharey=True)
-# fig.subplots_adjust(left=0.05, right=0.95, hspace=0.05, wspace=0.05)
-#
-#
-# def format_func(x, loc):
-#     if x == 0:
-#         return '0'
-#     elif x == 1:
-#         return 'h'
-#     elif x == -1:
-#         return '-h'
-#     else:
-#         return '%ih' % x
-#
-# for i, kernel in enumerate(['gaussian', 'tophat', 'epanechnikov',
-#                             'exponential', 'linear', 'cosine']):
-#     axi = ax.ravel()[i]
-#     log_dens = KernelDensity(kernel=kernel).fit(X_src).score_samples(X_plot)
-#     axi.fill(X_plot[:, 0], np.exp(log_dens), '-k', fc='#AAAAFF')
-#     axi.text(-2.6, 0.95, kernel)
-#
-#     axi.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
-#     axi.xaxis.set_major_locator(plt.MultipleLocator(1))
-#     axi.yaxis.set_major_locator(plt.NullLocator())
-#
-#     axi.set_ylim(0, 1.05)
-#     axi.set_xlim(-2.9, 2.9)
-#
-# ax[0, 1].set_title('Available Kernels')
+        # transform back into real space
+        axis.plot(scipy.special.expit(l_curve_x) * 100, l_curve_y, color='black', linestyle='-', linewidth=1,
+                  label='logit-norm')
 
-# ----------------------------------------------------------------------
-# # Plot a 1D density example
-# N = 100
-# np.random.seed(1)
-# X = np.concatenate((np.random.normal(0, 1, int(0.3 * N)),
-#                     np.random.normal(5, 1, int(0.7 * N))))[:, np.newaxis]
-#
-# X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
-#
-# true_dens = (0.3 * norm(0, 1).pdf(X_plot[:, 0])
-#              + 0.7 * norm(5, 1).pdf(X_plot[:, 0]))
-#
-# fig, ax = plt.subplots()
-# ax.fill(X_plot[:, 0], true_dens, fc='black', alpha=0.2,
-#         label='input distribution')
-# colors = ['navy', 'cornflowerblue', 'darkorange']
-# kernels = ['gaussian', 'tophat', 'epanechnikov']
-# lw = 2
-#
-# for color, kernel in zip(colors, kernels):
-#     kde = KernelDensity(kernel=kernel, bandwidth=0.5).fit(X)
-#     log_dens = kde.score_samples(X_plot)
-#     ax.plot(X_plot[:, 0], np.exp(log_dens), color=color, lw=lw,
-#             linestyle='-', label="kernel = '{0}'".format(kernel))
-#
-# ax.text(6, 0.38, "N={0} points".format(N))
-#
-# ax.legend(loc='upper left')
-# ax.plot(X[:, 0], -0.005 - 0.01 * np.random.random(X.shape[0]), '+k')
-#
-# ax.set_xlim(-4, 9)
-# ax.set_ylim(-0.02, 0.4)
-# plt.show()
+        # skew-normal
+        x2_skew = scipy.stats.skew(x)
+        x2_skew, x2_mean, x2_sigma = scipy.stats.skewnorm.fit(x, x2_skew)
+        curve_x = np.linspace(min_val, max_val, 1000)
+        curve_y = scipy.stats.skewnorm.pdf((curve_x - x2_mean) / x2_sigma, x2_skew) / x2_sigma
+        axis.plot(curve_x, len(x) * (max_val / num_bins) * curve_y, color='black', linestyle='--', linewidth=1,
+                  label='skewnorm')
+        # axis.axvline(x=x2_mean, color='g', linestyle='-', lw=4)
+
+        # gaussian
+        curve_x = np.linspace(min_val, max_val, 1001)
+        n_plot = len(x) * (max_val / num_bins) * scipy.stats.norm.pdf(curve_x, x_mean, np.sqrt(np.var(x)))
+        axis.plot(curve_x, n_plot, color='black', linestyle='-.', linewidth=1, label='gaussian')
+
+        # shade cutoff area
+        cut = 30  # score at which to cut off
+        axis.axvline(x=cut, color='0.7', linestyle='-')
+        axis.axvspan(0, cut, color='0.4', alpha=0.5)
+        count_remaining = sum(val > cut for val in x)
+        label += ': %d/%d (%0.2f%%) remaining' % (count_remaining, len(x), count_remaining * 100.0 / len(x))
+
+        # labels and formatting
+        axis.set_title(label, fontsize=font_size + 2)
+        axis.set_ylabel('count', fontsize=font_size)
+        axis.set_xlabel('score', fontsize=font_size)
+        axis.grid(True)
+        for tick_label in axis.get_yticklabels():
+            tick_label.set_fontsize(font_size - 1)
+        for tick_label in axis.get_xticklabels():
+            tick_label.set_fontsize(font_size - 1)
+
+        # fix y tick label to be actual amounts not probabilities
+        tick_labels = [int(tick) if i % 4 == 0 else '' for i, tick in enumerate(axis.get_yticks())]
+        axis.set_yticklabels(tick_labels)
+
+    fig.tight_layout()
+    plt.legend()
+
+    plt.show()
+
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+
+draw_histograms([X], ['x'])
